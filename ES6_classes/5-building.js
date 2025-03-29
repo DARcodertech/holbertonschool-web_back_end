f36d6a07f9b1a4353fbd993649f323210488dd85
root <root@DESKTOP-GV8C5Q5>
2025-03-29 14:08:32 +0100

    add

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,17 +1,12 @@
 export default class Building {
 constructor(sqft) {
-    if (this.constructor === Building) {
-        throw new Error("Building is an abstract class and cannot be instantiated directly");
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
 }
 
-
 get sqft() {
     return this._sqft;
-}
-
-evacuationWarningMessage() {
-    throw new Error("Class extending Building must override evacuationWarningMessage");
 }
 }
