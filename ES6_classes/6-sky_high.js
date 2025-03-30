87b44d9cd698272d87832a6c2df02c4478a1e53d
DARcodertech <9840@holbertonstudents.com>
2025-03-30 13:53:23 +0200

    Update 6-sky_high.js

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,22 +1,16 @@
-import Building from './5-building.js';
+import Building from './5-building';
 
-class SkyHighBuilding extends Building {
-constructor(sqft, floors) {
-super(sqft);
-this._floors = floors;
-}
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 
-get floors() {
-return this._floors;
-}
+  get floors() {
+    return this._floors;
+  }
 
-get sqft() {
-return this._sqft;
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
 }
-
-evacuationWarningMessage() {
-return `Evacuate slowly the ${this._floors} floors`;
-}
-}
-
-export default SkyHighBuilding;
