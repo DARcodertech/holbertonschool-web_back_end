1847a1fe8378e9eba469008ed15c6d2410a61f8a
root <root@DESKTOP-GV8C5Q5>
2025-03-30 13:30:59 +0200

    add

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,12 +1,14 @@
 export default class Building {
-constructor(sqft) {
-    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
+    constructor(sqft) {
+      this._sqft = sqft;
+      if (this.constructor !== Building) {
+        if (typeof this.evacuationWarningMessage !== 'function') {
+          throw new Error('Class extending Building must override evacuationWarningMessage');
+        }
+      }
     }
-    this._sqft = sqft;
-}
-
-get sqft() {
-    return this._sqft;
-}
-}
+  
+    get sqft() {
+      return this._sqft;
+    }
+  }
