4d51a40d2d97a8de15d4c9359b0ac4f2060d16c7
DARcodertech <9840@holbertonstudents.com>
2025-03-30 13:57:21 +0200

    Update 10-car.js

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,11 +1,13 @@
 export default class Car {
-    constructor(brand, motor, color) {
-        this._brand = brand;
-        this._motor = motor;
-        this._color = color;
-    }
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
 
-    cloneCar() {
-        return new this.constructor();
-    }
+  cloneCar() {
+    const Species = Symbol('Species');
+    this[Species] = this.constructor;
+    return new this[Species]();
+  }
 }
