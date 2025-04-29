c6a84e82b903e2c366bcafeb1bcc7c60eccfbe9d
DARcodertech <9840@holbertonstudents.com>
2025-04-29 11:46:05 +0200

    add

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+return a random number
+"""
+import asyncio
+import random
+
+async def async_generator():
+    """return a random number"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
