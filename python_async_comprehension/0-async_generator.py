a70f54fd82881f1b09536e72ef4dc19c22d2099f
DARcodertech <9840@holbertonstudents.com>
2025-04-30 11:11:33 +0200

    add

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@ return a random number
 """
 import asyncio
 import random
+from typing import Generator, AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """return a random number"""
     for _ in range(10):
         await asyncio.sleep(1)
